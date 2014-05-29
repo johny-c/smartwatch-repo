c8cf7422ecb87209ee20ff41263e1d29d5c5a7f9
kneidel <kneidel@in.tum.de>
2014-05-29 13:23:29 +0200

    Includes the lcd4linux project

---
+++ lcd4linux/config.h
@@ -0,0 +1,689 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to 1 if the `closedir' function returns void instead of `int'. */
+/* #undef CLOSEDIR_VOID */
+
+/* DBus Major Version */
+/* #undef DBUS_VERSION_MAJOR */
+
+/* DBus Micro Version */
+/* #undef DBUS_VERSION_MICRO */
+
+/* DBus Minor Version */
+/* #undef DBUS_VERSION_MINOR */
+
+/* Found some version of curses that we're going to use */
+/* #undef HAS_CURSES */
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the <asm/io.h> header file. */
+/* #undef HAVE_ASM_IO_H */
+
+/* Define to 1 if you have the <asm/types.h> header file. */
+#define HAVE_ASM_TYPES_H 1
+
+/* Define to 1 if you have the <dbus/dbus.h> header file. */
+/* #undef HAVE_DBUS_DBUS_H */
+
+/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
+   */
+#define HAVE_DIRENT_H 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+/* #undef HAVE_DLFCN_H */
+
+/* Define to 1 if you have the `dup2' function. */
+#define HAVE_DUP2 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `floor' function. */
+#define HAVE_FLOOR 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the <ftdi.h> header file. */
+/* #undef HAVE_FTDI_H */
+
+/* Define to 1 if you have the <gd/gd.h> header file. */
+/* #undef HAVE_GD_GD_H */
+
+/* Define to 1 if you have the <gd.h> header file. */
+/* #undef HAVE_GD_H */
+
+/* Define to 1 if you have the `gethostbyname' function. */
+#define HAVE_GETHOSTBYNAME 1
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the iconv() function and it works. */
+#define HAVE_ICONV 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+/* #undef HAVE_INTTYPES_H */
+
+/* Define to 1 if you have the <jpeglib.h> header file. */
+/* #undef HAVE_JPEGLIB_H */
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <libusb-1.0/libusb.h> header file. */
+/* #undef HAVE_LIBUSB_1_0_LIBUSB_H */
+
+/* Define to 1 if you have the <linux/dvb/frontend.h> header file. */
+#define HAVE_LINUX_DVB_FRONTEND_H 1
+
+/* Define to 1 if you have the <linux/errno.h> header file. */
+#define HAVE_LINUX_ERRNO_H 1
+
+/* Define to 1 if you have the <linux/hd44780.h> header file. */
+/* #undef HAVE_LINUX_HD44780_H */
+
+/* Define to 1 if you have the <linux/isdn.h> header file. */
+#define HAVE_LINUX_ISDN_H 1
+
+/* Define to 1 if you have the <linux/lcd-linux.h> header file. */
+/* #undef HAVE_LINUX_LCD_LINUX_H */
+
+/* Define to 1 if you have the <linux/parport.h> header file. */
+#define HAVE_LINUX_PARPORT_H 1
+
+/* Define to 1 if you have the <linux/ppdev.h> header file. */
+#define HAVE_LINUX_PPDEV_H 1
+
+/* Define to 1 if you have the <linux/wireless.h> header file. */
+#define HAVE_LINUX_WIRELESS_H 1
+
+/* Define to 1 if you have the <luise.h> header file. */
+/* #undef HAVE_LUISE_H */
+
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+/* #undef HAVE_MEMORY_H */
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <mysql/mysql.h> header file. */
+/* #undef HAVE_MYSQL_MYSQL_H */
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+/* #undef HAVE_NDIR_H */
+
+/* Define to 1 if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <net/if_ppp.h> header file. */
+#define HAVE_NET_IF_PPP_H 1
+
+/* Define to 1 if you have the <nmeap.h> header file. */
+/* #undef HAVE_NMEAP_H */
+
+/* Define to 1 if you have the `pow' function. */
+#define HAVE_POW 1
+
+/* Define to 1 if you have the `putenv' function. */
+#define HAVE_PUTENV 1
+
+/* If available, contains the Python version number currently in use. */
+/* #undef HAVE_PYTHON */
+
+/* Define to 1 if you have the `regcomp' function. */
+#define HAVE_REGCOMP 1
+
+/* Define to 1 if you have the <rfb/rfb.h> header file. */
+/* #undef HAVE_RFB_RFB_H */
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the <serdisplib/serdisp.h> header file. */
+/* #undef HAVE_SERDISPLIB_SERDISP_H */
+
+/* Define to 1 if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define to 1 if you have the <sqlite3.h> header file. */
+/* #undef HAVE_SQLITE3_H */
+
+/* Define to 1 if you have the `sqrt' function. */
+#define HAVE_SQRT 1
+
+/* Define to 1 if you have the <st2205.h> header file. */
+/* #undef HAVE_ST2205_H */
+
+/* Define to 1 if `stat' has the bug that it succeeds when given the
+   zero-length file name argument. */
+#define HAVE_STAT_EMPTY_STRING_BUG 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+/* #undef HAVE_STDINT_H */
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strcasecmp' function. */
+#define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define to 1 if you have the `strcspn' function. */
+#define HAVE_STRCSPN 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the `strftime' function. */
+#define HAVE_STRFTIME 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strncasecmp' function. */
+#define HAVE_STRNCASECMP 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the `strpbrk' function. */
+#define HAVE_STRPBRK 1
+
+/* Define to 1 if you have the `strrchr' function. */
+#define HAVE_STRRCHR 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the `strtol' function. */
+#define HAVE_STRTOL 1
+
+/* Define to 1 if you have the `strtoul' function. */
+#define HAVE_STRTOUL 1
+
+/* Define to 1 if you have the <syslog.h> header file. */
+#define HAVE_SYSLOG_H 1
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/io.h> header file. */
+#define HAVE_SYS_IO_H 1
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+/* #undef HAVE_SYS_STAT_H */
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+/* #undef HAVE_SYS_TYPES_H */
+
+/* Define to 1 if you have the <sys/vfs.h> header file. */
+#define HAVE_SYS_VFS_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the `uname' function. */
+#define HAVE_UNAME 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <usb.h> header file. */
+#define HAVE_USB_H 1
+
+/* Define to 1 if you have the `vfork' function. */
+#define HAVE_VFORK 1
+
+/* Define to 1 if you have the <vfork.h> header file. */
+/* #undef HAVE_VFORK_H */
+
+/* Define to 1 if `fork' works. */
+#define HAVE_WORKING_FORK 1
+
+/* Define to 1 if `vfork' works. */
+#define HAVE_WORKING_VFORK 1
+
+/* Define to 1 if you have the <X11/Xlib.h> header file. */
+/* #undef HAVE_X11_XLIB_H */
+
+/* Define to 1 if you have the <X11/Xutil.h> header file. */
+/* #undef HAVE_X11_XUTIL_H */
+
+/* Define as const if the declaration of iconv() needs const. */
+#define ICONV_CONST 
+
+/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
+   slash. */
+/* #undef LSTAT_FOLLOWS_SLASHED_SYMLINK */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+/* #undef LT_OBJDIR */
+
+/* Set to reflect version of ncurses */
+/* #undef NCURSES_970530 */
+
+/* If you Curses does not have color define this one */
+/* #undef NO_COLOR_CURSES */
+
+/* Name of package */
+#define PACKAGE "lcd4linux"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "lcd4linux-users@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "LCD4Linux"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "LCD4Linux 0.11.0-SVN"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "lcd4linux"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.11.0-SVN"
+
+/* apm plugin */
+#define PLUGIN_APM 1
+
+/* asterisk plugin */
+#define PLUGIN_ASTERISK 1
+
+/* button_exec plugin */
+#define PLUGIN_BUTTON_EXEC 1
+
+/* cpuinfo plugin */
+#define PLUGIN_CPUINFO 1
+
+/* dbus plugin */
+/* #undef PLUGIN_DBUS */
+
+/* diskstats plugin */
+#define PLUGIN_DISKSTATS 1
+
+/* dvb plugin */
+#define PLUGIN_DVB 1
+
+/* event plugin */
+#define PLUGIN_EVENT 1
+
+/* exec plugin */
+#define PLUGIN_EXEC 1
+
+/* fifo plugin */
+#define PLUGIN_FIFO 1
+
+/* file plugin */
+#define PLUGIN_FILE 1
+
+/* gps plugin */
+/* #undef PLUGIN_GPS */
+
+/* hddtemp plugin */
+#define PLUGIN_HDDTEMP 1
+
+/* huawei plugin */
+#define PLUGIN_HUAWEI 1
+
+/* i2c sensors plugin */
+#define PLUGIN_I2C_SENSORS 1
+
+/* iconv charset converter plugin */
+#define PLUGIN_ICONV 1
+
+/* imon plugin */
+#define PLUGIN_IMON 1
+
+/* ISDN plugin */
+#define PLUGIN_ISDN 1
+
+/* kvv plugin */
+#define PLUGIN_KVV 1
+
+/* loadavg plugin */
+#define PLUGIN_LOADAVG 1
+
+/* meminfo plugin */
+#define PLUGIN_MEMINFO 1
+
+/* mpd plugin */
+/* #undef PLUGIN_MPD */
+
+/* mpris_dbus plugin */
+/* #undef PLUGIN_MPRIS_DBUS */
+
+/* mysql plugin */
+/* #undef PLUGIN_MYSQL */
+
+/* netdev plugin */
+#define PLUGIN_NETDEV 1
+
+/* netinfo plugin */
+#define PLUGIN_NETINFO 1
+
+/* POP3 plugin */
+#define PLUGIN_POP3 1
+
+/* ppp plugin */
+#define PLUGIN_PPP 1
+
+/* proc_stat plugin */
+#define PLUGIN_PROC_STAT 1
+
+/* python plugin */
+/* #undef PLUGIN_PYTHON */
+
+/* qnaplog plugin */
+/* #undef PLUGIN_QNAPLOG */
+
+/* raspi plugin */
+#define PLUGIN_RASPI 1
+
+/* sample plugin */
+#define PLUGIN_SAMPLE 1
+
+/* seti plugin */
+#define PLUGIN_SETI 1
+
+/* statfs plugin */
+#define PLUGIN_STATFS 1
+
+/* uname plugin */
+#define PLUGIN_UNAME 1
+
+/* uptime plugin */
+#define PLUGIN_UPTIME 1
+
+/* w1retap plugin */
+#define PLUGIN_W1RETAP 1
+
+/* wireless plugin */
+#define PLUGIN_WIRELESS 1
+
+/* xmms plugin */
+#define PLUGIN_XMMS 1
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* Define if you want to turn on SCO-specific code */
+/* #undef SCO_FLAVOR */
+
+/* Define to the type of arg 1 for `select'. */
+#define SELECT_TYPE_ARG1 int
+
+/* Define to the type of args 2, 3 and 4 for `select'. */
+#define SELECT_TYPE_ARG234 (fd_set *)
+
+/* Define to the type of arg 5 for `select'. */
+#define SELECT_TYPE_ARG5 (struct timeval *)
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Use Ncurses? */
+/* #undef USE_NCURSES */
+
+/* Use SunOS SysV curses? */
+/* #undef USE_SUNOS_CURSES */
+
+/* Use SystemV curses? */
+/* #undef USE_SYSV_CURSES */
+
+/* Version number of package */
+#define VERSION "0.11.0-SVN"
+
+/* ASTUSB driver */
+#define WITH_ASTUSB 1
+
+/* Beckmann&Egle driver */
+#define WITH_BECKMANNEGLE 1
+
+/* BWCT driver */
+#define WITH_BWCT 1
+
+/* Crystalfontz driver */
+#define WITH_CRYSTALFONTZ 1
+
+/* Curses driver */
+/* #undef WITH_CURSES */
+
+/* CwLinux driver */
+#define WITH_CWLINUX 1
+
+/* D4D driver */
+#define WITH_D4D 1
+
+/* Define if using the dmalloc debugging malloc package */
+/* #undef WITH_DMALLOC */
+
+/* DPF driver */
+#define WITH_DPF 1
+
+/* Electronic Assembly RS232 graphic driver */
+#define WITH_EA232graphic 1
+
+/* Driver for EFN LED modules and EUG 100 ethernet to serial converter */
+#define WITH_EFN 1
+
+/* FutabaVFD driver */
+#define WITH_FUTABAVFD 1
+
+/* Allnet FW8888 driver */
+#define WITH_FW8888 1
+
+/* G-15 driver */
+#define WITH_G15 1
+
+/* GD library */
+/* #undef WITH_GD */
+
+/* GLCD2USB driver */
+#define WITH_GLCD2USB 1
+
+/* HD44780 driver */
+#define WITH_HD44780 1
+
+/* I2C bus driver */
+#define WITH_I2C 1
+
+/* image widget */
+/* #undef WITH_IMAGE */
+
+/* IRLCD driver */
+#define WITH_IRLCD 1
+
+/* LCD2USB driver */
+#define WITH_LCD2USB 1
+
+/* LCD-Linux driver */
+/* #undef WITH_LCDLINUX */
+
+/* LCDTerm driver */
+#define WITH_LCDTERM 1
+
+/* LEDMatrix driver */
+#define WITH_LEDMATRIX 1
+
+/* LPH7508 driver */
+#define WITH_LPH7508 1
+
+/* LUIse driver */
+/* #undef WITH_LUISE */
+
+/* LW ABP driver */
+#define WITH_LW_ABP 1
+
+/* M50530 driver */
+#define WITH_M50530 1
+
+/* MatrixOrbital driver */
+#define WITH_MATRIXORBITAL 1
+
+/* MatrixOrbitalGX driver */
+#define WITH_MATRIXORBITALGX 1
+
+/* MDM166A driver */
+/* #undef WITH_MDM166A */
+
+/* Milford Instruments driver */
+#define WITH_MILINST 1
+
+/* Newhaven driver */
+#define WITH_NEWHAVEN 1
+
+/* Noritake driver */
+#define WITH_NORITAKE 1
+
+/* NULL driver */
+#define WITH_NULL 1
+
+/* parport bus driver */
+#define WITH_PARPORT 1
+
+/* Pertelian driver */
+#define WITH_PERTELIAN 1
+
+/* PHAnderson driver */
+#define WITH_PHANDERSON 1
+
+/* PICGraphic driver */
+#define WITH_PICGRAPHIC 1
+
+/* picoLCD driver */
+#define WITH_PICOLCD 1
+
+/* picoLCDGraphic driver */
+#define WITH_PICOLCDGRAPHIC 1
+
+/* PNG driver */
+/* #undef WITH_PNG */
+
+/* PPM driver */
+#define WITH_PPM 1
+
+/* RouterBoard driver */
+#define WITH_ROUTERBOARD 1
+
+/* Sample driver */
+#define WITH_SAMPLE 1
+
+/* SamsungSPF driver */
+/* #undef WITH_SAMSUNGSPF */
+
+/* serdisplib driver */
+/* #undef WITH_SERDISPLIB */
+
+/* serial bus driver */
+#define WITH_SERIAL 1
+
+/* ShuttleVFD driver */
+#define WITH_SHUTTLEVFD 1
+
+/* SimpleLCD driver */
+#define WITH_SIMPLELCD 1
+
+/* st2205 driver */
+/* #undef WITH_ST2205 */
+
+/* T6963 driver */
+#define WITH_T6963 1
+
+/* TeakLCM driver */
+#define WITH_TEAK_LCM 1
+
+/* TREFON driver */
+#define WITH_TREFON 1
+
+/* ULA200 driver */
+/* #undef WITH_ULA200 */
+
+/* USBHUB driver */
+#define WITH_USBHUB 1
+
+/* USBLCD driver */
+#define WITH_USBLCD 1
+
+/* vnc driver */
+/* #undef WITH_VNC */
+
+/* WincorNixdorf driver */
+#define WITH_WINCORNIXDORF 1
+
+/* X11 driver */
+/* #undef WITH_X11 */
+
+/* Define to 1 if the X Window System is missing or not being used. */
+#define X_DISPLAY_MISSING 1
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `long int' if <sys/types.h> does not define. */
+/* #undef off_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
+
+/* Define as `fork' if `vfork' does not work. */
+/* #undef vfork */
